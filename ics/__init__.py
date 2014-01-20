727a57ebf53a0cd34793a4010c68404c53083321
Gregory Ellis <gellis@glsonline.com>
2014-01-19 18:09:14 -0800

    + add initial handling of tasks(Todos) from ics file:   + add ics file parsing   + add couple of Todo convenience methods: is_complete,is_overdue,fancy_due + - TODO:   - add Todo tests!!!   - Todo properties: verify correct logic/operation vs. RFC5545, particularly:   	   duration, due, percent, completed,   - update Event tests w.r.t. new Todo/TodoList additions   - add more Todo.properties per RFC spec:   - Address my design questions w/ N. Marchan:       - ICS File Parsing Failures: 	      - Plans to fall out gracefully / send an Exception back up the stack? 		  - How to determine/display the offending Component programatically w/o 		      print statements? 		  - How do you want to handle Categories: as list or string?   - Dump all extra/debugging comments/code

--- ics/__init__.py
+++ ics/__init__.py
@@ -5,6 +5,7 @@ from __future__ import unicode_literals, absolute_import
 
 from .icalendar import Calendar
 from .event import Event
+from .todo import Todo
 from .__meta__ import (
     __title__,
     __version__,
